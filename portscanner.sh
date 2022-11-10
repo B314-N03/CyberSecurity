f5f1ba6d9be7657146ca73f95300d6ba25ed513b
Bela Noe <bela-noe@web.de>
2022-11-10 22:06:36 +0100

    idk

--- portscanner.sh
+++ portscanner.sh
@@ -13,7 +13,7 @@ done
 
 if [ -z  $FileName  ]; then
     echo
-else 
+else
     FileName=$FileName.txt
     touch  $FileName
 fi
@@ -22,10 +22,11 @@ YoN=false
 # List of Ports
 # Option 1
 top_10="80 23 554 3306 179 1080 445 161 162 443"
-# Option 2
+# Option 2 
+# Sum = 37
 top_50="80 23 554 3306 179 1080 445 161 162 443 7 20 21 22 25 53 69 88 102 110 135 137 139 143 381 383 465 587 593 636 691 902 989 990 995 1025 1194 "
 # Option 3
-top_100="23 554 3306 445 80 5432 443"
+top_100="80 23 554 3306 179 1080 445 161 162 443 7 20 21 22 25 53 69 88 102 110 135 137 139 143 381 383 465 587 593 636 691 902 989 990 995 1025 1194"
 
 # Set the color variable
 green='\033[0;32m'
@@ -54,7 +55,7 @@ echo Results are not being saved!
 	# Port Scan via NetCat without saving it to a file
 	# Iterate through PORTS List to Scan those given Ports at the given IP
 	echo
-	echo Please be patient this could take a while, depending on your Internet Speed! 
+	echo Please be patient this could take a while, depending on your Internet Speed!
 	echo " "
 		for port in ${PORTS[@]};do
  			echo Checking $IP:$port
@@ -69,7 +70,7 @@ echo Results are not being saved!
 		done
 else
 	# normal='echo -en "\e[0m"'
-	#Check the Location of the File
+	# Check the Location of the File
 	echo Results are being saved to $FileName!
 
 	# Port Scan via NetCat and saving it to a .txt file 
